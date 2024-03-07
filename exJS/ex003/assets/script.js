3a6917607a88e82f97ad44be6f159f85cce92386
Pablo Leão Dias dos Santos <151883726+PabloLeaoDev@users.noreply.github.com>
2024-03-07 11:20:17 -0300

    ex003 de js finalizado

--- exJS/ex003/assets/script.js
+++ exJS/ex003/assets/script.js
@@ -2,15 +2,15 @@
 // captar o operador
 // fazer a operação especficada com os dois inputs
 // mostrar o resultado na tela
-let str1 = window.document.querySelector('#n1');
-let num1 = Number(str1.value);
-let str2 = window.document.querySelector('#n2');
-let num2 = Number(str2.value);
-let opt = window.document.querySelector('#op').value;
-let rtd = window.document.querySelector('#res');
-let op;
+function calcular() {
+    let str1 = window.document.querySelector('#n1');
+    let num1 = Number(str1.value);
+    let str2 = window.document.querySelector('#n2');
+    let num2 = Number(str2.value);
+    let opt = window.document.querySelector('#seletor').value;
+    let rtd = window.document.querySelector('#res');
+    let op;
 
-function entrada() {
     if (opt === '+') {
         op = num1 + num2;
     } else if (opt === '-') {
@@ -23,5 +23,3 @@ function entrada() {
     
     rtd.innerHTML = op;
 }
-
-
