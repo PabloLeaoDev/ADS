7d8005473cbc3860845131a006196a7bef2dd6a8
Pablo Leao <devpablol@gmail.com>
2024-04-26 11:04:09 -0300

    main5.py atualizado
    Melhorei o código, para torná-lo mais performático e limpo.

--- aula-logica-de-programacao-algoritmos/main5.py
+++ aula-logica-de-programacao-algoritmos/main5.py
@@ -10,26 +10,24 @@ time.sleep(1)
 type = input('Qual é o seu tipo de instalação? ')
 hwh = int(input('Quanto foi o consumo de energia, em kWh? '))
 
-if (type == 'R' or type == 'r'):
-    if (hwh <= 500):
-        valor = hwh * 0.40
-        print(f'O valor a ser pago é de R${valor:.2f}')
-    else:
-        valor = hwh * 0.65
-        print(f'O valor a ser pago é de R${valor:.2f}')
-elif (type == 'C' or type == 'c'):
-    if (hwh <= 1000):
-        valor = hwh * 0.55
-        print(f'O valor a ser pago é de R${valor:.2f}')
-    else:
-        valor = hwh * 0.60
-        print(f'O valor a ser pago é de R${valor:.2f}')
-elif (type == 'I' or type == 'i'):
-    if (hwh <= 5000):
-        valor = hwh * 0.55
-        print(f'O valor a ser pago é de R${valor:.2f}')
-    else:
-        valor = hwh * 0.60
-        print(f'O valor a ser pago é de R${valor:.2f}')
-else:
+if (type.upper() not in ['R', 'C', 'I']):
+    # se o tipo de instalação não for R, C ou I
     print('Tipo de instalação inválido!')
+else:
+    valor = 0
+    if (type.upper() == 'R'):
+        if (hwh <= 500):
+            valor = hwh * 0.40
+        else:
+            valor = hwh * 0.65
+    elif (type.upper() == 'C'):
+        if (hwh <= 1000):
+            valor = hwh * 0.55
+        else:
+            valor = hwh * 0.60
+    elif (type.upper() == 'I'):
+        if (hwh <= 5000):
+            valor = hwh * 0.55
+        else:
+            valor = hwh * 0.60
+    print(f'O valor a ser pago é de R${valor:.2f}')
