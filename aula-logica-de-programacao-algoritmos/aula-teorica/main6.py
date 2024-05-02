1ce46ef43f635f97a8796fb5fafb3988094398ad
Pablo Leão Dias dos Santos <devpablol@gmail.com>
2024-05-02 10:46:09 -0300

    Update main6.py

--- aula-logica-de-programacao-algoritmos/aula-teorica/main6.py
+++ aula-logica-de-programacao-algoritmos/aula-teorica/main6.py
@@ -1,8 +1,11 @@
 # função que cria uma borda personalizada para a string informada
 def border(string):
-    phrase = len(string)
-    print('+' + '-' * phrase + '+')
-    print('|' + string + '|')
-    print('+' + '-' * phrase + '+')
+    if string:
+        phrase = len(string)
+        print('\n' + '+' + '-' * phrase + '+')
+        print('|' + string + '|')
+        print('+' + '-' * phrase + '+')
+    else:
+        print('\nVocê não escreveu uma frase.')
     
 border(input('Escreva uma frase: '))
