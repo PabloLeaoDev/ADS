b32c6981597a61a8ee7986c739c71a3f28b03c34
Pablo Leão Dias dos Santos <devpablol@gmail.com>
2024-05-03 13:12:39 -0300

    main9.py criado
    Aprendendo sobre dicionários, listas, tuplas e strings.

---
+++ aula-logica-de-programacao-algoritmos/aula-teorica/main9.py
@@ -0,0 +1,4 @@
+games = {'nome': ['Super Mário', 'GTA 5', 'Call of Duty'], 'ano': [1985, 2013, 2023], 'plataforma': ['Nitendo', 'Desktop', 'PlayStation 5']}
+
+for i in range(len(games)):
+    print('O jogo', games['nome'][i], 'foi criado em', games['ano'][i], 'e jogado na plataforma', games['plataforma'][i])
